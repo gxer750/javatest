259081f41f8ccc3ab25882713a4225752468b470
gxer750 <911gegnaw@gmail.com>
2022-05-04 17:06:16 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//int num = 12;
+      //  num *= 2;
+        //num -= 4;
+        //num++;
+        boolean status = 7 < 8;
+       //int endpoint = 11 % 3;
+       // String username1 = "teracoder";
+        //String username2 = "gigacoder";
+        System.out.println(8 <= 8);
+       // System.out.println(username1.equals(username2));
+    }
+}
+
---
+++ src/com/company/test3/Main.java
@@ -0,0 +1,19 @@
+package com.company.test3;
+
+public class Main {
+    public static void main(String[] args) {
+        // Magic number
+
+        int myNumber = 3;
+        //myNumber is the original number
+
+        int stepOne = 3 * 3;
+        int stepTwo = 6 + 3;
+        int stepThree = 9 / 3;
+        int stepFour = 3 + 17;
+        int stepFive = 20 - 3;
+        int stepSix = 17 / 6;
+
+        System.out.println(stepThree);
+    }
+}
